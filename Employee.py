33952a1643316b5002a91ec4e8d0bc0a03d1d25d
sonnig <kallolymess@yahoo.com>
2018-06-01 02:08:39 +0200

    Static Methods : Methods that dont take Class or an Instance as a first option

--- Employee.py
+++ Employee.py
@@ -1,8 +1,11 @@
+import datetime
+
 class Employee():
 
-#  How class methods can be used a new Constructor
-# Take for instance if we get the data about a new employee in the form 
-# Jason-Bourn-40000, John-Doe-50000, Steve-Stoned-60000, Jane-Doe-50000
+# Static Methods : 
+# Regular Methods pass an Instance as the first option, 
+# Class Methods pass a Class as the first option, 
+# Static Methods pass None
  
     num_of_emps = 0
     raise_ammount = 1.02
@@ -30,35 +33,32 @@ class Employee():
 
     @classmethod
     def from_string(cls, emp_string):
-# split the string 
         first, last, pay = emp_string.split('-')
-# make new Employee with them
         return cls(first, last, pay)
+
+# They dont have a class or an instance as the first option
+# They are mostly some function that is relevant to this Class and so its there.
+# A sure way to know if you should use a Static Method instead of Class or Regular Method is
+# to see if the 'cls' or 'self' is being referred to at all inside the method
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 emp1 = Employee('Kallol', 'Biswas', 40000)
 emp2 = Employee('Corey', 'Schafer', 50000)
 emp3 = Employee('Test', 'User', 60000)
 
-
-# If we were to create Employee instances out of the THAT data we would have to do 
-# something like set them to variables
 emp_str1 = 'Jason-Bourn-40000'
 emp_str2 = 'John-Doe-50000'
 emp_str3 = 'Steve-Stoned-60000'
 emp_str4 = 'Jane-Doe-50000'
 
-# split the string 
 first, last, pay = emp_str1.split('-')
-
-# make new Employee with them
 emp4 = Employee(first, last, pay)
-
-# Test
 print(emp4.__dict__)
 
-# Troublesome and accident prone, Automation is the answer.
-
-# So we make a new new Classmethod that returns a Employee when we feed it a string like John-Doe-50000
 emp5 = Employee.from_string(emp_str2)
 emp6 = Employee.from_string(emp_str3)
 emp7 = Employee.from_string(emp_str4)
@@ -66,4 +66,10 @@ print(emp5.__dict__)
 print(emp6.__dict__)
 print(emp7.__dict__)
 
- 
+my_date = datetime.date(2016, 7, 11)
+# Calling a static method.
+print(Employee.is_workday(my_date))
+
+# on a sidenote the imported datetime module, datetime.py will convert a lot of 
+# different ways of date expressions, that is being done with multiple Constructors 
+# similar to what we have here as second Constructor 'from_string'
