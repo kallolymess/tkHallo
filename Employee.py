2427c3e39e9b3d4ffceb286bfb72432567664ac0
sonnig <kallolymess@yahoo.com>
2018-06-01 23:25:18 +0200

    dunders repr and str

--- Employee.py
+++ Employee.py
@@ -16,56 +16,27 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_ammount)
 
-# We can see that just this will fully create a subclass
-class Developer(Employee):
-    raise_ammount = 1.1
+# Dunders are methods surrounded by "__". 
+# __repr__ is an unambiguous representation of an object and should be used for debugging and logging purposes
+# __str__ is a printable representation of the object usually meant for end users.
 
-# Creating the constructor for the Subclass, and passing the 
-# class variables to the super class, in this case to class Employee 
-    def __init__(self, first, last, pay, prog_lang):
-        super().__init__(first, last, pay)
-# This is equivalent to Employee.__init__(self, first, last, pay)
-        self.prog_lang = prog_lang
+    def __repr__(self):
+        return "Employee('{}', '{}', '{}')".format(self.first, self.last, self.pay)
 
-class Manager(Employee):
-    def __init__(self, first, last, pay, employees=None):
-        super().__init__(first, last, pay)
-        if employees is None:
-            self.employees = []
-        else:
-            self.employees = employees
+    def __str__(self):
+        return '{} - {}'.format(self.fullname(), self.email)        
 
-    def add_emp(self, emp):
-        if emp not in self.employees:
-            self.employees.append(emp)
-            
-    def remove_emp(self, emp):
-        if emp in self.employees:
-            self.employees.remove(emp)
+emp1 = Employee('Robertsonn', 'Jabberwalkie', 40000)
+emp2 = Employee('Truely', 'Duely', 50000)
+emp3 = Employee('Susan', 'Smith', 60000)
 
-    def print_emps(self):
-        for emp in self.employees:
-            print('--> ', emp.fullname())
+# print(emp1) will try to find the __str__ first for that object
+# if that is not found it will try to fall back to __repr__.
+print(emp1)    
 
-# Test Subclassing
-emp1 = Developer('Kallol', 'Biswas', 40000, 'python')
-emp2 = Developer('Corey', 'Schafer', 50000, 'Java')
-emp3 = Employee('Dave', 'Dey', 60000)
-            
-mgr_1 = Manager('Trudy', 'Manager', 80000, [emp1])
-
-print(mgr_1.email)
-mgr_1.add_emp(emp2)
-mgr_1.add_emp(emp3)
-mgr_1.print_emps()
-print('-------')
-mgr_1.remove_emp(emp3)
-mgr_1.print_emps()
-
-print(isinstance(mgr_1, Employee))
-print(isinstance(mgr_1, Manager))
-print(isinstance(mgr_1, Developer))
-
-print(issubclass(Manager, Employee))
-print(issubclass(Employee, Developer))
-print(issubclass(Developer, Employee))
+# it is however always possible to print out the __str__ and __repr__ explicitly if needed
+print(repr(emp2))
+print(str(emp3))
+# or we can do
+print(emp1.__repr__())
+print(emp1.__str__())
