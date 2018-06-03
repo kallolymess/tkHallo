f51c9cdb65d3f75dbd32fd456ac1f6349a96157a
KB <kallolymess@yahoo.com>
2018-06-03 21:42:15 +0200

    Lists Tuples Sets

---
+++ ListTuplesSets.py
@@ -0,0 +1,33 @@
+courses = ['Math', 'Physics', 'History', 'CompSci', 'Biology']
+
+
+# print(courses.index('History'))
+
+# nums = [11, 2, 9, 88, 4, 32]
+
+# print(nums)
+# print(sorted(nums))
+# print(sum(nums))
+# print(min(nums))
+# print(max(nums))
+
+# sorted(courses)
+# print(courses)
+# print(sorted(courses))
+# print(courses)
+
+# slicing
+# print(courses[1:2])
+# print(courses[2:])
+
+# courses.append('Art')
+# courses.insert(2, 'Commerce')
+# print(courses)
+# courses.remove('History')
+# print(courses)
+
+# courses.sort()
+# print(courses)
+
+# courses.reverse()
+# print(courses)
